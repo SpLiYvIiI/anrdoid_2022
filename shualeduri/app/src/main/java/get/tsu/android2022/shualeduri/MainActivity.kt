98d9d52f3cf41b2169108b2fe6cac6d3690656fe
SpLiYvIiI <atukadagio123@gmail.com>
2022-05-15 20:38:25 +0400

    re-submit ;(

--- shualeduri/app/src/main/java/get/tsu/android2022/shualeduri/MainActivity.kt
+++ shualeduri/app/src/main/java/get/tsu/android2022/shualeduri/MainActivity.kt
@@ -1,40 +1,16 @@
 package get.tsu.android2022.shualeduri
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import get.tsu.android2022.shualeduri.databinding.LoginPageBinding
-import get.tsu.android2022.shualeduri.databinding.ProfilePageBinding
-import kotlinx.android.synthetic.main.login_page.view.*
-import kotlinx.android.synthetic.main.profile_page.view.*
+import android.os.Bundle
+import get.tsu.android2022.shualeduri.fragments.LoginFragment
 
-private lateinit var loginPageBinding: LoginPageBinding
-private lateinit var profilePageBinding: ProfilePageBinding
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        loginPageBinding = LoginPageBinding.inflate(layoutInflater)
-        profilePageBinding = ProfilePageBinding.inflate(layoutInflater)
-        val loginPageRoot = loginPageBinding.root
-        val profilePageRoot = profilePageBinding.root
-        setContentView(loginPageRoot)
+        setContentView(R.layout.activity_main)
+        val loginFragment = LoginFragment()
 
-        // init/implement event listeners
-        loginPageRoot.loginButtonImage.setOnClickListener{
-            profilePageRoot.hello_text.text = "Hello, " + loginPageRoot.profile_name.text
-            setContentView(profilePageRoot);
-            loginPageRoot.profile_name.setText("enter your name")
-        }
-
-        loginPageRoot.loginImage.setOnClickListener{
-            profilePageRoot.hello_text.text = "Hello, " + loginPageRoot.profile_name.text
-            setContentView(profilePageRoot);
-            loginPageRoot.profile_name.setText("enter your name")
-        }
-
-        profilePageRoot.log_out.setOnClickListener{
-            profilePageRoot.hello_text.text = "";
-            setContentView(loginPageRoot)
-        }
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container,loginFragment).commit()
     }
 }
